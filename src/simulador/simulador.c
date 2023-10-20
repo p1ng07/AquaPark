05c0a4bd0999bdb6aa63b0529fc563ce4e23e9ee
p1ng07 <franciscoppontes369@gmail.com>
2023-10-20 21:48:27 +0100

    Added: Receber nome de ficheiro de configuração ler por argumento e exemplo de leitura de configuração

--- src/simulador/simulador.c
+++ src/simulador/simulador.c
@@ -1,6 +1,25 @@
+#include "../common/configuration.h"
+#include "../common/string.h"
 #include "print.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(void) {
-  print_simul();
+int main(int argc, char* argv[]) {
+
+  if (argc < 2){
+    printf("ERRO: Não foi especificado um ficheiro de configuração");
+    return 1;
+  }
+
+  configuration conf = extract_config_from_file(argv[1]);
+
+  conf_parameter *param = get_parameter_from_configuration(
+      &conf, new_str("negativo"));
+
+  if(param != NULL){
+    printf("%d", param->i);
+  }
+
   return 0;
 }
