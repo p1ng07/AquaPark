bc52e2087dcc10802deb0d94b5c63023fc7e22d0
p1ng07 <franciscoppontes369@gmail.com>
2023-10-20 21:47:45 +0100

    Added: str construct

--- src/common/string.c
+++ src/common/string.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "string.h"
 
 void str_print(str string){
@@ -7,3 +9,34 @@ void str_print(str string){
   }
   printf("\n");
 }
+
+int str_equals(str* str1, str* str2){
+  if (str1->length != str2->length){
+    return 0;
+  }
+
+  for (int i = 0; i < str1->length; i++) {
+    if (str1->value[i] != str2->value[i]) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+str* new_str(char* null_term_string){
+  str* string = malloc(sizeof(str));
+
+  char *heap_allocated_string = malloc(sizeof(char) * strlen(null_term_string));
+  strcpy(heap_allocated_string, null_term_string);
+
+  string->length = strlen(null_term_string);
+  string->value = heap_allocated_string;
+  return string;
+}
+
+void str_free(str* str){
+  free(str->value);
+  free(str);
+}
+
--- src/common/string.h
+++ src/common/string.h
@@ -10,4 +10,16 @@ typedef struct {
 /* Summary: Escreve uma str terminada em '\n' para a consola 
  */
 void str_print(str string);
+
+/* Summary: Compara duas str
+   Retorna: 1 se ambas são iguais, 0 se são diferentes
+ */
+int str_equals(str* str1, str* str2);
+
+/*
+  AVISO: O apontador retornado terá de ser manualmente libertado com str_free()
+ */
+str* new_str(char* null_term_string);
+
+void str_free(str* str);
 #endif
